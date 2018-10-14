a0ff64296385bcd83e789cf9e154fc2da8654b7d
sergivb01 <sergivb01@gmail.com>
2018-10-14 19:42:21 +0200

    remove unnecessary eslint-disable comment

--- src/middlewares.js
+++ src/middlewares.js
@@ -4,7 +4,6 @@ function notFound(req, res, next) {
   next(error)
 }
 
-/* eslint-disable-next-line no-unused-vars */
 function errorHandler(err, req, res, next) {
   const statusCode = res.statusCode !== 200 ? res.statusCode : 500
   res.status(statusCode)
